89feab563b97b1981f9aed318981c6e6f598630e
Code Hz <CodeHz@outlook.com>
2019-05-08 00:05:35 +0800

    improved promise

--- include/promise.hpp
+++ include/promise.hpp
@@ -2,13 +2,26 @@
 #include <functional>
 #include <type_traits>
 
-template <typename T, typename R = void> struct _fn { using type = std::function<R(T const &)>; };
+template <typename T> class promise;
 
-template <typename R> struct _fn<void, R> { using type = std::function<R()>; };
+template <typename T> struct promise_ref { using type = T const &; };
+template <typename T> struct promise_ref<promise<T>> { using type = T &&; };
+
+template <typename T, typename R = void> struct void_fn { using type = std::function<R(T const &)>; };
+template <typename R> struct void_fn<void, R> { using type = std::function<R()>; };
+template <typename T, typename R = void> using void_fn_t = typename void_fn<T, R>::type;
+
+template <typename T> struct unpromise;
+template <typename T> struct unpromise<promise<T>> { using type = T; };
+template <typename T> using unpromise_t = typename unpromise<T>::type;
+
+template <typename T> struct is_promise { constexpr static auto value = false; };
+template <typename T> struct is_promise<promise<T>> { constexpr static auto value = true; };
+template <typename T> constexpr auto is_promise_v = is_promise<T>::value;
 
 template <typename T> class promise {
-  using then_fn                            = typename _fn<T>::type;
-  template <typename R> using transform_fn = typename _fn<T, R>::type;
+  using then_fn                            = void_fn_t<T>;
+  template <typename R> using transform_fn = void_fn_t<T, R>;
   using fail_fn                            = std::function<void(std::exception const &ex)>;
 
   then_fn _then;
@@ -30,27 +43,56 @@ public:
 
     friend class promise;
   };
+  promise(const promise &) = delete;
+  promise &operator=(const promise &) = delete;
+  promise(promise &&rhs)
+      : body(rhs.body)
+      , _then(rhs._then)
+      , _fail(rhs._fail) {
+    rhs.body = nullptr;
+  }
+  promise &operator=(promise &&rhs) {
+    body     = rhs.body;
+    rhs.body = nullptr;
+    _then    = rhs._then;
+    _fail    = rhs._fail;
+  }
   promise(std::function<void(then_fn, fail_fn)> f)
       : body(f) {}
   promise(std::function<void(resolver)> f)
       : body([=](auto th, auto fn) {
         f({ th, fn });
       }){};
-  ~promise() {}
-
+  ~promise() {
+    if (body) {
+      if constexpr (std::is_void_v<T>) {
+        body(
+            _then ?: []() {}, _fail ?: [](auto) {});
+      } else {
+        body(
+            _then ?: [](T const &) {}, _fail ?: [](auto) {});
+      }
+    }
+  }
   promise<T> &then(then_fn _then) {
     this->_then = _then;
     return *this;
   }
   template <typename R> promise<R> then(transform_fn<R> fn) {
-    return { [=, next = body](auto th, auto fa) { next([=](T const &t) { th(fn(t)); }, fa); } };
+    auto next = body;
+    body      = nullptr;
+    if constexpr (std::is_void_v<T>)
+      return { [=](auto th, auto fa) { next([=]() { th(fn()); }, fa); } };
+    else
+      return { [=](auto th, auto fa) { next([=](T const &t) { th(fn(t)); }, fa); } };
   }
   promise<T> &fail(fail_fn) {
     this->_fail = _fail;
     return *this;
   }
-  void operator()() {
-    body(
-        _then ?: [](auto...) {}, _fail ?: [](auto) {});
+  T force() {
+    auto next = body;
+    body      = nullptr;
+    return { [=](auto th, auto fa) { next([=](T const &t) { const_cast<T &>(t).then(th).fail(fa); }, fa); } };
   }
 };
--- src/test-client.cpp
+++ src/test-client.cpp
@@ -10,13 +10,21 @@ int main() {
   try {
     static RPC::Client client(std::make_unique<client_wsio>("ws://127.0.0.1:16400/"));
     client.start()
-        .then([] {
+        .then<promise<json>>([] {
           std::cout << "ready!" << std::endl;
-          client.call("test", json::array({ "test" })).then([](json data) { std::cout << "recv: " << data.dump(2) << std::endl; }).fail([](auto ex) {
-            std::cerr << typeid(ex).name() << ex.what() << std::endl;
-          })();
+          return client.call("test", json::array({ "test" }));
         })
-        .fail([](auto ex) { std::cerr << typeid(ex).name() << ex.what() << std::endl; })();
+        .force()
+        .then<promise<json>>([](json data) {
+          std::cout << "recv: " << data.dump(2) << std::endl;
+          return client.call("test", json::array({ "test2" }));
+        })
+        .force()
+        .then([&](json data) {
+          std::cout << "recv: " << data.dump(2) << std::endl;
+          client.stop();
+        })
+        .fail([](auto ex) { std::cerr << typeid(ex).name() << ex.what() << std::endl; });
 
   } catch (std::runtime_error &e) { std::cerr << typeid(e).name() << e.what() << std::endl; }
 }
